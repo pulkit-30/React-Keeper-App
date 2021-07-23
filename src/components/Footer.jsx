1f5a09b2566b0c88ff1caa02e3bc8463b4653523
pulkit <pulit30.bsr@gmail.com>
2021-07-23 22:10:13 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="foot">
+      <div>Made with love</div>
+      <div>By Pulkit Gupta</div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NotesIcon from "@material-ui/icons/Notes";
+function Header() {
+  return (
+    <div>
+      <header>
+        <NotesIcon /> Keeper
+      </header>
+    </div>
+  );
+}
+
+export default Header;
